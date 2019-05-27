195b2b97aae4993f6c332eb7ff939f9f01d5882e
farithgomez <farith.gomez@gmail.com>
2019-05-27 00:20:44 -0600

    Validations in input and search
    Validations added to form input and table search.

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Person } from '../person';
 //import { Subject } from 'rxjs';
 import { PERSONS } from '../mock-persons';
+import { stringify } from '@angular/compiler/src/util';
 
 class DataTablesResponse {
   data: any[];
@@ -27,6 +28,7 @@ export class PersonsComponent implements OnDestroy, OnInit {
 
   model: Person = new Person();
   submitted = false;
+  
 
   onSubmit() { this.submitted = true }
 
@@ -49,6 +51,7 @@ export class PersonsComponent implements OnDestroy, OnInit {
       pageLength: 5,
       serverSide: false,
       processing: true,
+      data: this.foundPersons,
       columns: [
         { data: 'id' }, 
         { data: 'firstName' }, 
@@ -59,9 +62,11 @@ export class PersonsComponent implements OnDestroy, OnInit {
       ]
     };
 
-  }
+  }//END ON INIT
 
   loadTable(): void {
+
+    $('#personsTable').DataTable().destroy();
 
     this.dtOptions = {
       pagingType: 'full_numbers',
@@ -79,69 +84,196 @@ export class PersonsComponent implements OnDestroy, OnInit {
       ]
     };
 
+    $('#personsTable').DataTable();
+
   }
 
   searchPersons(): void {
 
     this.foundPersons = [];
 
-    for(let i=0; i < this.persons.length; i++){
+    if(this.searchTerm == undefined){
+      this.foundPersons = this.persons;
 
-      let person = this.persons[i];
+    }else{
 
-      //firstName
-      if(person.firstName.toUpperCase().includes(this.searchTerm.toUpperCase())){
+      for(let i=0; i < this.persons.length; i++){
+
+        let person = this.persons[i];
+
+        //firstName
+        if(person.firstName.toUpperCase().includes(this.searchTerm.toUpperCase())){
+            this.foundPersons.push(person);
+            continue;
+        }
+        //lastName
+        if(person.lastName.toUpperCase().includes(this.searchTerm.toUpperCase())){
+            this.foundPersons.push(person);
+            continue;
+        }
+
+        //full name
+        var fullName = person.firstName + ' ' + person.lastName;
+        if(fullName.toUpperCase().includes(this.searchTerm.toUpperCase())){
           this.foundPersons.push(person);
           continue;
-      }
-      //lastName
-      if(person.lastName.toUpperCase().includes(this.searchTerm.toUpperCase())){
-          this.foundPersons.push(person);
-          continue;
-      }
+        }
 
-      //address
-      if(person.address.toUpperCase().includes(this.searchTerm.toUpperCase())){
-          this.foundPersons.push(person);
-          continue;
-      }
+        //address
+        if(person.address.toUpperCase().includes(this.searchTerm.toUpperCase())){
+            this.foundPersons.push(person);
+            continue;
+        }
 
-      //mail
-      if(person.mail.toUpperCase().includes(this.searchTerm.toUpperCase())){
-          this.foundPersons.push(person);
-          continue;
-      }
+        //mail
+        if(person.mail.toUpperCase().includes(this.searchTerm.toUpperCase())){
+            this.foundPersons.push(person);
+            continue;
+        }
 
-      //phone
-      if(person.phone.toString().includes(this.searchTerm.toString())){
-          this.foundPersons.push(person);
-      }
+        //phone
+        if(person.phone.toString().includes(this.searchTerm.toString())){
+            this.foundPersons.push(person);
+        }
 
-    }//END FOR
+      }//END FOR
+    }
     
     this.loadTable();
 
   }
 
+  isEmail(email: string) {
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if(!regex.test(email)) {
+      return false;
+    }else{
+      return true;
+    }
+  }
+
   checkAddPerson(): void {
 
     var newPerson = new Person();
+    var isFormValid = false;
+    var isFirstNameValid = false;
+    var isLastNameValid = false;
+    var isAddressValid = false;
+    var isMailValid = false;
+    var isPhoneValid = false;
 
-    newPerson.id = this.model.id;
-    newPerson.firstName = this.model.firstName;
-    newPerson.lastName = this.model.lastName;
-    newPerson.address = this.model.address;
-    newPerson.mail = this.model.mail;
-    newPerson.phone = this.model.phone;
+    //first name
+    if(this.model.firstName == undefined || this.model.firstName == ''){
+      $('#valFirstName').show();
+      isFirstNameValid = false;
+    }else{
+      $('#valFirstName').hide();
 
-    this.persons.push(newPerson);
-    
-    //this.dtOptions.destroy;
-    //this.loadTable();
+      if(this.checkNumbers(this.model.firstName)){
+        $('#formatFirstName').show();
+        isFirstNameValid = false;
+      }else{
+        $('#formatFirstName').hide();
+        isFirstNameValid = true;
+      }
+ 
+    }
 
-    this.model = new Person();
-    this.model.id = this.persons.length + 1;
+    //last name
+    if(this.model.lastName == undefined || this.model.lastName == ''){
+      $('#valLastName').show();
+      isLastNameValid = false;
+    }else{
+      $('#valLastName').hide();
 
+      if(this.checkNumbers(this.model.lastName)){
+        $('#formatLastName').show();
+        isLastNameValid = false;
+      }else{
+        $('#formatLastName').hide();
+        isLastNameValid = true;
+      }
+
+    }
+
+    //address
+    if(this.model.address == undefined || this.model.address == ''){
+      $('#valAddress').show();
+      isAddressValid = false;
+    }else{
+      $('#valAddress').hide();
+      isAddressValid = true;
+    }
+
+    //mail
+    if(this.model.mail == undefined || this.model.mail == ''){
+      $('#valMail').show();
+      isMailValid = false;
+    }else{
+      $('#valMail').hide();
+
+      if(this.isEmail(this.model.mail)){
+        isMailValid = true;
+        $('#formatMail').hide();
+      }else{
+        isMailValid = false;
+        $('#formatMail').show();
+      }
+
+
+    }
+
+    //phone
+    if(this.model.phone == undefined || this.model.phone == ''){
+      $('#valPhone').show();
+      isPhoneValid = false;
+    }else{
+      $('#valPhone').hide();
+
+      if($.isNumeric(this.model.phone)){
+        isPhoneValid = true;
+        $('#formatPhone').hide();
+      }else{
+        isPhoneValid = false;
+        $('#formatPhone').show();
+      }
+
+      
+    }
+
+    if(isFirstNameValid && isLastNameValid && isAddressValid && isMailValid && isPhoneValid){
+      isFormValid = true;
+    }
+
+    if(isFormValid){
+      newPerson.id = this.model.id;
+      newPerson.firstName = this.model.firstName;
+      newPerson.lastName = this.model.lastName;
+      newPerson.address = this.model.address;
+      newPerson.mail = this.model.mail;
+      newPerson.phone = this.model.phone;
+
+      this.persons.push(newPerson);
+      
+      //this.dtOptions.destroy;
+      //this.loadTable();
+
+      this.model = new Person();
+      this.model.id = this.persons.length + 1;
+
+      alert('Person added successfully');
+    }
+  }
+
+  checkNumbers(value: string){
+
+    for(let i=0; i<value.length; i++){
+      if($.isNumeric(value[i].toString())){
+        return true;
+      }
+    }
+
+    return false;
   }
 
   ngOnDestroy(): void {
